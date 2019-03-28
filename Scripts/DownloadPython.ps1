961d193cbf1be3521712e45e8a612ac3b3287e29
M <Mohammed10work@gmail.com>
2019-03-27 20:35:20 -0400

    Fix DPython.ps1

--- Scripts/DownloadPython.ps1
+++ Scripts/DownloadPython.ps1
@@ -24,8 +24,7 @@ $wgetBinLocation = get_wget
 # Downlaod
 $filename = "$_" + ".msi"
 $Location = '' + $save_dir + '\' + $filename;
-Start-Process -FilePath $wgetBinLocation -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O $Location" -passthru -N
-oNewWindow -Wait
+Start-Process -FilePath $wgetBinLocation -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O $Location" -passthru -NoNewWindow -Wait
 # Install
 Start-Process -FilePath "msiexec.exe"  -Args "/a $Location /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
 }
@@ -34,10 +33,8 @@ Start-Process -FilePath "msiexec.exe"  -Args "/a $Location /qb TARGETDIR=C:\pyth
 
 #$filename = "wget.exe"
 #$Location = '' + $save_dir + '\' + $filename;
-#Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\2.msi
-" -passthru -NoNewWindow -Wait -WindowStyle hidden
-#Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\3.msi
-" -passthru -NoNewWindow -Wait -WindowStyle hidden
+#Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\2.msi" -passthru -NoNewWindow -Wait -WindowStyle hidden
+#Start-Process -FilePath $wgetBinLocation  -Args " https://www.python.org/ftp/python/2.7/python-2.7.msi -O C:\Windows\Temp\3.msi" -passthru -NoNewWindow -Wait -WindowStyle hidden
 
 #Start-Process -FilePath "msiexec.exe"  -Args "/a $Location /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
 #Start-Process -FilePath "msiexec.exe"  -Args "/a C:\Windows\Temp\2.msi /qb TARGETDIR=C:\python27" -passthru -NoNewWindow -Wait
