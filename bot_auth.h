c79c7985fbd569ffbf3d1ff515f3cd02631f906d
Mohammed <38591075+M507@users.noreply.github.com>
2019-04-28 19:05:11 -0700

    Create bot_auth.h

---
+++ bot_auth.h
@@ -0,0 +1,28 @@
+//
+// Created by M on 4/28/2019.
+//
+
+#include <winsock2.h>
+#include <stdio.h>
+#include <time.h>
+#pragma comment(lib, "w2_32")
+
+
+char *getmyip(char *IP){
+#define TMP_FILE_NAME "TMP_IP.txt"
+
+    system("ipconfig | findstr IPv4 > TMP_IP.txt");
+    FILE * fdir = fopen(TMP_FILE_NAME, "r");
+    char buff[500];
+    if (fdir){
+        while (1) {
+            if (fgets(buff, 300, fdir) == NULL) break;
+            IP = strcat(IP,buff);
+            //printf("%s", buff);
+        }
+    }
+    fclose(fdir);
+    remove(TMP_FILE_NAME);
+    return IP;
+}
+
